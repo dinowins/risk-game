97d5f869b37c766af1064032b40a1180cee631dd
Student <example@example.com>
2019-03-18 14:36:05 -0700

    add correct breakpoint for USA set-interval JS.

--- src/user-interface-logic.js
+++ src/user-interface-logic.js
@@ -2,17 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import './scss/styles.scss';
-import { dice } from "./../src/business-logic.js"; 
+import { dice } from "./../src/business-logic.js";
 
 $(document).ready(function() {
   $(".usa").click(function(event) {
-    setInterval(function() {
-      let roll = dice();
-      let murica = parseInt($("#usa-tot").text());
-      let completeUsa = murica - roll;
-      console.log(roll);
-      console.log(completeUsa);
-      $("#usa-tot").text(completeUsa);
-    }, 1000);
+    let completeUsa = 10;
+    // let completeEurope = 10;
+      setInterval(function() {
+        if (completeUsa >= 1) {
+          $("#usa-tot").text(completeUsa);
+          let murica = parseInt($("#usa-tot").text());
+          let roll = dice();
+          completeUsa = murica - roll;
+          console.log(roll);
+          console.log(completeUsa);
+        } else {
+          clearInterval();
+          completeUsa = 0;
+          $("#usa-tot").text(completeUsa);
+        }
+      }, 1000);
   })
 });
