bc6c942432818766e0653df5fec52748fe5b5e6b
Student <example@example.com>
2019-03-18 16:37:51 -0700

    add messaged out possibilities.

--- src/business-logic.js
+++ src/business-logic.js
@@ -1,4 +1,39 @@
+const usa = new squareRolls(usa, 50);
+const eu = new squareRolls(eu, 50);
+const southAm = new squareRolls(southAm, 50);
+const africa = new squareRolls(africa, 50);
+
 export let dice = () => {
     let randomNumber = Math.floor(Math.random() * 6) + 1;
     return randomNumber;
 }
+
+export class squareRolls {
+  constructor(square, numbers) {
+    this.square = square;
+    this.numbers = numbers;
+  }
+
+  selectedSquare() {
+    if ()
+  }
+}
+
+
+// export let roll = (first, second) => {
+//   let usaRoll = () => {
+//     if (completeUsa >= 1) {
+//       if (completeEurope <= 0) {
+//         clearInterval(usaRoll);
+//       }
+//       $("#usa-tot").text(completeUsa);
+//       let roll = dice();
+//       completeUsa = completeUsa - roll;
+//       console.log("USA roll " + roll);
+//     } else {
+//       clearInterval(usaRoll);
+//       completeUsa = 0;
+//       $("#usa-tot").text(completeUsa);
+//     }
+//   }
+// }
--- src/user-interface-logic.js
+++ src/user-interface-logic.js
@@ -7,36 +7,39 @@ import { dice } from "./../src/business-logic.js";
 $(document).ready(function() {
   $(".usa").click(function(event) {
     let completeUsa = 30;
-    let completeEurope = 10;
-      let usaRoll = setInterval(function() {
-        if (completeUsa >= 1) {
-          if (completeEurope <= 0) {
-            clearInterval(usaRoll);
-          }
-          $("#usa-tot").text(completeUsa);
-          let murica = parseInt($("#usa-tot").text());
-          let roll = dice();
-          completeUsa = murica - roll;
-          console.log("USA roll " + roll);
-        } else {
-          clearInterval();
-          completeUsa = 0;
-          $("#usa-tot").text(completeUsa);
-        }
-      }, 1000);
+    let completeEurope = 15;
 
-      let europeRoll = setInterval(function() {
-        if (completeEurope >= 1) {
-          $("#euro-tot").text(completeEurope);
-          let euro = parseInt($("#euro-tot").text());
-          let roll = dice();
-          completeEurope = euro - roll;
-          console.log("Europe roll: " + roll);
-        } else {
+    let europeRoll = setInterval(function() {
+      if (completeEurope >= 1) {
+        if (completeUsa <= 0) {
           clearInterval(europeRoll);
-          completeEurope = 0;
-          $("#euro-tot").text(completeEurope);
         }
-      }, 1000);
+        $("#euro-tot").text(completeEurope);
+        let roll = dice();
+        completeEurope = completeEurope - roll;
+        console.log("Europe roll: " + roll);
+      } else {
+        clearInterval(europeRoll);
+        completeEurope = 0;
+        $("#euro-tot").text(completeEurope);
+      }
+    }, 1000);
+
+    let usaRoll = setInterval(function() {
+      if (completeUsa >= 1) {
+        if (completeEurope <= 0) {
+          clearInterval(usaRoll);
+        }
+        $("#usa-tot").text(completeUsa);
+        let roll = dice();
+        completeUsa = completeUsa - roll;
+        console.log("USA roll " + roll);
+      } else {
+        clearInterval(usaRoll);
+        completeUsa = 0;
+        $("#usa-tot").text(completeUsa);
+      }
+    }, 1000);
+
   })
 });
