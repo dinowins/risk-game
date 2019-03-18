a6eab71155e416f0ce9b2d2f7a4bc8283e4c9856
Student <example@example.com>
2019-03-18 13:58:29 -0700

    Add setInterval to dice UI

--- src/user-interface-logic.js
+++ src/user-interface-logic.js
@@ -2,14 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import './scss/styles.scss';
-import { dice } from "./../src/business-logic.js";
+import { dice } from "./../src/business-logic.js"; 
 
 $(document).ready(function() {
   $(".usa").click(function(event) {
-    let roll = dice.roll();
-    let murica = parseInt($("#usa-tot").text());
-    let completeUsa = murica - roll;
-    console.log(completeUsa);
-    $("#usa-tot").text(completeUsa);
+    setInterval(function() {
+      let roll = dice();
+      let murica = parseInt($("#usa-tot").text());
+      let completeUsa = murica - roll;
+      console.log(roll);
+      console.log(completeUsa);
+      $("#usa-tot").text(completeUsa);
+    }, 1000);
   })
 });
